ee99ce74df573b7ff80af7c5e0f9c30dcd83eb75
Luis Chiappe <chiappe.luis@gmail.com>
2015-02-17 04:35:17 -0300

    jquery update + scss

--- js/function.js
+++ js/function.js
@@ -1,23 +1,21 @@
 $(document).ready(function(){
     
-    // Animation
-    var dropdown = $('.dropdown ul').hide();
-    
-    $('a.arrow').click(function(){
-        $(dropdown).slideToggle();
+    // Animation    
+    $('.dropdown__box__arrow').click(function(){
+        $('.dropdown__list').slideToggle();
     });
 
     // Text first item
     var firstItem = $('.dropdown__list .dropdown__list__item:first-child').text();
-	$('.dropdown__box span').append(firstItem);
+	$('.dropdown__box .dropdown__box__text').append(firstItem);
 
 	// Change text item
     $('.dropdown__list .dropdown__list__item').click(function(e){
     	e.preventDefault();
     	var text = $(this).text();
  
-    	$('.dropdown__box span').text("");
-    	$('.dropdown__box span').append(text);
+    	$('.dropdown__box .dropdown__box__text').text("");
+    	$('.dropdown__box .dropdown__box__text').append(text);
     });
     
 });
